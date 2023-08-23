e4719b9973e3770efcc168b5a6eb01cd9ef29eb9
Damodar-Naik <82937522+Damodar-Naik@users.noreply.github.com>
2023-08-24 00:01:23 +0530

    chat privately with the connected users

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.static(__dirname + '/public'));
 // Store user data (name and private chat info)
 const users = {};
 
+// Store messages for private chats
+const privateMessages = {};
+
 io.on('connection', (socket) => {
     console.log('A user connected');
 
@@ -20,11 +23,22 @@ io.on('connection', (socket) => {
     socket.on('user name', (name) => {
         users[socket.id] = { name, privateChat: null, socketId: socket.id };
         io.emit('user list', Object.values(users));
+        privateMessages[socket.id] = {}; // Initialize messages for this user
     });
 
     // Handle private chat requests
     socket.on('private chat', (targetUserId) => {
         users[socket.id].privateChat = targetUserId;
+        if (!privateMessages[socket.id][targetUserId]) {
+            privateMessages[socket.id][targetUserId] = []; // Initialize messages for this private chat
+        }
+        if (!privateMessages[targetUserId]) {
+            privateMessages[targetUserId] = {};
+        }
+        if (!privateMessages[targetUserId][socket.id]) {
+            privateMessages[targetUserId][socket.id] = [];
+        }
+        io.to(socket.id).emit('private messages', privateMessages[socket.id][targetUserId]);
     });
 
     // Handle private messages
@@ -33,20 +47,18 @@ io.on('connection', (socket) => {
         const target = users[data.targetUserId];
 
         if (sender.privateChat === data.targetUserId && target) {
-            io.to(data.targetUserId).emit('private message', {
-                text: data.text,
-                senderName: sender.name,
-            });
+            const message = { text: data.text, senderName: sender.name };
+            privateMessages[socket.id][data.targetUserId].push(message);
+            privateMessages[data.targetUserId][socket.id].push(message);
 
-            io.to(socket.id).emit('private message', {
-                text: data.text,
-                senderName: sender.name,
-            });
+            io.to(data.targetUserId).emit('private message', message);
+            io.to(socket.id).emit('private message', message);
         }
     });
 
     socket.on('disconnect', () => {
         delete users[socket.id];
+        delete privateMessages[socket.id];
         io.emit('user list', Object.values(users));
         console.log('A user disconnected');
     });
